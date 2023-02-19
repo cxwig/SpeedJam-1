c7da68cfe2f7fc30361c7d772f348d50ab578803
DenDenDor <dendor333@gmail.com>
2023-02-20 02:15:56 +0300

    Box

--- SpeedJam1/Assets/Scripts/GodCode/Movement/Enemy/ChangerEnemyState.cs
+++ SpeedJam1/Assets/Scripts/GodCode/Movement/Enemy/ChangerEnemyState.cs
@@ -11,6 +11,7 @@ public class ChangerEnemyState
     private Transform _target;
     private SetterSneakingMove _setterSneakingMove;
     private bool _canFollow;
+    private IMove _lastMove;
     public ChangerEnemyState(SetterSneakingMove setterSneakingMove, Transform transform, Transform target, float range, IMove followongTargetMove, IMove partolMove)
     {
         _setterSneakingMove = setterSneakingMove;
@@ -19,13 +20,22 @@ public class ChangerEnemyState
         _range = range;
         _followingTargetMove = followongTargetMove;
         _partolMove = partolMove;
+        _lastMove = _partolMove;
     }
 
     public IMove GetMove(IMove followongTargetMove, IMove partolMove)
     {
-        bool canFollow = Vector2.Distance(_transform.position, _target.position) < _range && (_setterSneakingMove.IsSneaking == false || _canFollow);
-        _canFollow = canFollow;
+        //  bool canFollow = Vector2.Distance(_transform.position, _target.position) < _range && (_setterSneakingMove.IsSneaking == false || _canFollow);
+        // _canFollow = canFollow;
         //Debug.LogError(canFollow + " OLOL! IS SNEAKING: " + _setterSneakingMove.IsSneaking);
-       return canFollow ? followongTargetMove : partolMove;
+        // return canFollow ? followongTargetMove : partolMove;
+        Debug.Log(_setterSneakingMove.IsSneaking + " HAHAH!");
+        if (Vector2.Distance(_transform.position, _target.position) < _range && (_lastMove.GetType() == followongTargetMove.GetType() || _setterSneakingMove.IsSneaking))
+        {
+            _lastMove = _followingTargetMove;
+            return followongTargetMove;
+        }
+        _lastMove = partolMove;
+        return partolMove;
     }
 }
