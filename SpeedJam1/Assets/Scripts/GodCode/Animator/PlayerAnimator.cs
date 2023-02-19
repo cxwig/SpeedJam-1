ae285b6b1aef1e246fa302dfe0fdc406a77336ee
DenDenDor <dendor333@gmail.com>
2023-02-19 11:52:36 +0300

    Animation.

---
+++ SpeedJam1/Assets/Scripts/GodCode/Animator/PlayerAnimator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private GetterPlayerMove _getterPlayerMove;
+    [SerializeField] private ChangerPlayerState _changerPlayerState;
+    private bool _isMoving;
+    private void Awake()
+    {
+        _changerPlayerState.OnSneak += PlaySneakAnimation;
+        _changerPlayerState.OnMove += PlaySimpleAnimation;
+    }
+    private void PlaySneakAnimation()
+    {
+        _animator.SetBool("isSneaking", true);
+    }
+    private void PlaySimpleAnimation()
+    {
+        Debug.Log("HOORAY!");
+        _animator.SetBool("isSneaking", false);        
+    }
+    private void Update()
+    { 
+         _isMoving = _getterPlayerMove.Move.ReturnerVector.ReturnVector() == Vector3.zero;
+        _animator.SetBool("isMoving", _isMoving);
+    }
+    private void OnDisable()
+    {
+        _changerPlayerState.OnSneak -= PlaySneakAnimation;
+        _changerPlayerState.OnMove -= PlaySimpleAnimation;
+    }
+}
