952d3c61aa568b6bd4f99cc526cdbe0e2919d347
Vasilii <vasilii.orlov1905@gmail.com>
2023-02-19 00:45:48 -0800

    Settings Menu Done
    ya sleep

---
+++ SpeedJam1/Assets/Scripts/GodCode/UI/SettingsMenu/Helpers/MenuToggler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AkshayDhotre.GraphicSettingsMenu
+{
+    public class MenuToggler : MonoBehaviour
+    {
+        public Canvas menuCanvasComponent;
+
+
+        public void ButtonCheckSettings()
+        {
+                if (menuCanvasComponent.enabled)
+                {
+                    SetMenuActive(false);
+                }
+                else
+                {
+                    SetMenuActive(true);
+                    
+                }
+        }
+
+        /// <param name="active"></param>
+        public void SetMenuActive(bool active)
+        {
+            menuCanvasComponent.enabled = active;
+        }
+    }
+}
+
